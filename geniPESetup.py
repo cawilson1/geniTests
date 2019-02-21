38867d45e0934381e51f87735aadeb35236b7869
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-02-20 19:14:32 -0500

    Add files via upload

---
+++ geniPESetup.py
@@ -0,0 +1,38 @@
+#imports
+import os
+import pexpect
+
+#prepare and open the file 
+os.system("hostname > file.txt")
+temp_file = open("file.txt.","r")
+name = ""
+for line in temp_file:
+        name = line[:line.index(".")]
+
+print(name)
+temp_file.close()
+os.system("sudo rm file.txt")
+
+#opening various ports
+os.system("sudo iptables -A INPUT -p udp --dport 12345 -j ACCEPT")
+os.system("sudo iptables -A INPUT -P udp --dport 6512 -j ACCEPT")
+os.system("sudo iptables -A INPUT -p tcp --dport 578 -j ACCEPT")
+os.system("sudo iptables -A INPUT -p tcp --dport 2957 -j ACCEPT")
+#standard telnet port
+os.system("sudo iptables -A INPUT -p tcp --dport 23 -j ACCEPT")
+#standard ssh port
+os.system("sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT")
+
+#creating a vulnerable user and password
+os.system("sudo useradd jDoe")
+child = pexpect.spawn("sudo passwd jDoe")
+child.expect("Enter new UNIX password: ")
+child.sendline("password")
+child.expect("Retype new UNIX password: ")
+child.sendline("password")
+
+#creating a hidden file with the open ports in it
+os.system("echo \"Open Ports: \n12345 \n6512 \n578 \n2957 \n23 \n22\" | cat > .openPorts.txt")
+
+
+
