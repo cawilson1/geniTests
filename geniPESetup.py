7eb732b9b1a85c809afcd37a049170591bfd16cd
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-03-04 18:04:23 -0500

    Update geniPESetup.py

--- geniPESetup.py
+++ geniPESetup.py
@@ -8,8 +8,11 @@ os.system("sudo iptables -A INPUT -p udp --dport 12345 -j ACCEPT")
 os.system("sudo iptables -A INPUT -P udp --dport 6512 -j ACCEPT")
 os.system("sudo iptables -A INPUT -p tcp --dport 578 -j ACCEPT")
 os.system("sudo iptables -A INPUT -p tcp --dport 2957 -j ACCEPT")
-#standard telnet port
+#open telnet port and prepare it for telneting
 os.system("sudo iptables -A INPUT -p tcp --dport 23 -j ACCEPT")
+os.system("sudo apt-get install telnetd")
+os.system("cd /etc/init.d")
+os.system("sudo inetd restart")
 #standard ssh port
 os.system("sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT")
 
@@ -21,8 +24,14 @@ child.sendline("xX53cUrEXx")
 child.expect("Retype new UNIX password: ")
 child.sendline("xX53cUrEXx")
 
-#creating a hidden file with the open ports in it
-os.system("echo \"The passphrase is CanadaIsGreat" | cat > .pass.txt")
+#creating a hidden file with the stegnographic in it
+os.system("echo \"The passphrase is CanadaIsGreat\" | cat > .pass.txt")
+os.system("wget https://raw.githubusercontent.com/Setzlerte/geniTests/master/coolSpace.jpg")
 
+
+#Locking down the machine for access from the KALI machine
+os.system("iptables -A INPUT -j REJECT")
+os.system("iptables -A OUTPUT -j ACCEPT")
+os.system("iptables -A OUTPUT -o lo -j ACCEPT")
 
 
