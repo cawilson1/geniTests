6748cef06f084fcc12c092aef52788eb7feabcca
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-03-12 18:00:33 -0400

    Update geniPESetup.py

--- geniPESetup.py
+++ geniPESetup.py
@@ -8,6 +8,7 @@ os.system("iptables -P INPUT DROP")
 os.system("iptables -P FORWARD DROP")
 os.system("iptables -P OUTPUT ACCEPT")
 os.system("iptables -A INPUT -s 10.10.1.1 -j ACCEPT")
+os.system("iptables -A INPUT -s 10.10.1.2 -j ACCEPT")
 """
 #opening various ports
 os.system("sudo iptables -A INPUT -p udp --dport 12345 -j ACCEPT")
