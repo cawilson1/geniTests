db433e886a0bcb5e22e1cfa3edabefbe4c22be3a
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-03-15 15:02:11 -0400

    Update geniPESetup.py

--- geniPESetup.py
+++ geniPESetup.py
@@ -5,7 +5,7 @@ import pexpect
 
 #Locking down the machine for access from the KALI machine
 os.system("iptables -P INPUT DROP")
-os.system("iptables -P FORWARD DROP")
+#os.system("iptables -P FORWARD DROP")
 os.system("iptables -P OUTPUT ACCEPT")
 os.system("iptables -A INPUT -s 10.10.1.1 -j ACCEPT")
 os.system("iptables -A INPUT -s 10.10.1.2 -j ACCEPT")
