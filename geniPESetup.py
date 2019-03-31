f8a13f63e01284c585c95f908fdc041dcc0ed996
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-03-31 13:22:58 -0400

    Update geniPESetup.py

--- geniPESetup.py
+++ geniPESetup.py
@@ -67,6 +67,25 @@ os.system("sudo wget -P /home/jDoe/Pictures/Wallpapers https://raw.githubusercon
 os.system("sudo wget -P /home/jDoe/Pictures/Wallpapers https://raw.githubusercontent.com/Setzlerte/geniTests/master/bulkFiles/mooseLights.jpeg")
 os.system("sudo wget -P /home/jDoe/Pictures/Wallpapers https://raw.githubusercontent.com/Setzlerte/geniTests/master/bulkFiles/spaceJar.jpeg")
 
+#preparing the file for SSH
+
+count = 1
+ports = []
+
+for line in ssh_config_file:
+  if count == 55:
+		new_ssh_config_file.write("PasswordAuthentication yes")
+	else:
+		new_ssh_config_file.write(line)
+	if "Port" in line:
+		ports.append(line[line.index(" ") + 1:-1])
+		count += 1
+
+ssh_config_file.close()
+new_ssh_config_file.close()
+
+os.system("sudo mv /etc/ssh/sshd_config_2 /etc/ssh/sshd_config")
+os.system("sudo service ssh restart")
 
 
 
