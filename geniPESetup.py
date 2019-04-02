893c5bb08958dfd5ae845d7d0501dd7f998bbf92
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-04-01 20:33:39 -0400

    Update geniPESetup.py

--- geniPESetup.py
+++ geniPESetup.py
@@ -2,17 +2,17 @@
 import os
 import pexpect
 
-
+"""
 #Locking down the machine for access from the KALI machine
 ##os.system("iptables -P INPUT DROP")
-#os.system("iptables -P FORWARD DROP")
+os.system("iptables -P FORWARD DROP")
 os.system("iptables -P OUTPUT ACCEPT")
 os.system("iptables -A INPUT -s 10.10.1.1 -j ACCEPT")
 os.system("iptables -A INPUT -s 10.10.1.2 -j ACCEPT")
 #opening input from port 3000 so that the OWASP juiceshop can run
 os.system("iptables -A INPUT -p tcp --dport 3000 -j ACCEPT")
 os.system("iptables -A INPUT -p udp --dport 3000 -j ACCEPT")
-
+"""
 #opening various ports
 os.system("sudo iptables -A INPUT -p udp --dport 12345 -j ACCEPT")
 os.system("sudo iptables -A INPUT -P udp --dport 6512 -j ACCEPT")
@@ -71,22 +71,32 @@ os.system("sudo wget -P /home/jDoe/Pictures/Wallpapers https://raw.githubusercon
 ssh_config_file = open("/etc/ssh/sshd_config", "r")
 new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
 
-count = 1
-ports = [
-]
+ssh_config_file = open("/etc/ssh/sshd_config", "r")
+	new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
 
-for line in ssh_config_file:
-	if count == 55:
-		new_ssh_config_file.write("PasswordAuthentication yes")
-	else:
-		new_ssh_config_file.write(line)
-	count += 1
-ssh_config_file.close()
-new_ssh_config_file.close()
+	count = 1
 
-os.system("sudo mv /etc/ssh/sshd_config_2 /etc/ssh/sshd_config")
-os.system("sudo service ssh restart")
+	ports = [
+	]
 
+	for line in ssh_config_file:
+		if count == 55:
+			new_ssh_config_file.write("PasswordAuthentication yes")
+		else:
+			new_ssh_config_file.write(line)
+
+		if "Port" in line:
+			ports.append(line[line.index(" ") + 1:-1])
+		count += 1
+
+	ssh_config_file.close()
+	new_ssh_config_file.close()
+
+	os.system("sudo mv /etc/ssh/sshd_config_2 /etc/ssh/sshd_config")
+	os.system("sudo service ssh restart")
+
+	for port in ports:
+		os.system("sudo iptables -A INPUT -p tcp --dport " + port + " -j DROP")
 
 
 
