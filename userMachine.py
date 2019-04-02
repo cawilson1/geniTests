c3faa98d5ee225fc72e955edd4ab3eab8a496cca
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-04-02 19:29:20 -0400

    Update userMachine.py

--- userMachine.py
+++ userMachine.py
@@ -1,25 +1,12 @@
-import os
-import pexpect
-
 ssh_config_file = open("/etc/ssh/sshd_config", "r")
 new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
 
-ssh_config_file = open("/etc/ssh/sshd_config", "r")
-	new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
-
 	count = 1
-
-	ports = [
-	]
-
 	for line in ssh_config_file:
 		if count == 55:
 			new_ssh_config_file.write("PasswordAuthentication yes")
 		else:
 			new_ssh_config_file.write(line)
-
-		if "Port" in line:
-			ports.append(line[line.index(" ") + 1:-1])
 		count += 1
 
 	ssh_config_file.close()
@@ -27,8 +14,5 @@ ssh_config_file = open("/etc/ssh/sshd_config", "r")
 
 	os.system("sudo mv /etc/ssh/sshd_config_2 /etc/ssh/sshd_config")
 	os.system("sudo service ssh restart")
-
-	for port in ports:
-		os.system("sudo iptables -A INPUT -p tcp --dport " + port + " -j DROP")
 
 
