0c2050c32ef386265f73bf3f4410ee056bf8599b
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-04-02 17:56:40 -0400

    Update geniPESetup.py

--- geniPESetup.py
+++ geniPESetup.py
@@ -68,37 +68,6 @@ os.system("sudo wget -P /home/jDoe/Pictures/Wallpapers https://raw.githubusercon
 os.system("sudo wget -P /home/jDoe/Pictures/Wallpapers https://raw.githubusercontent.com/Setzlerte/geniTests/master/bulkFiles/spaceJar.jpeg")
 
 #preparing the file for SSH
-ssh_config_file = open("/etc/ssh/sshd_config", "r")
-new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
-
-ssh_config_file = open("/etc/ssh/sshd_config", "r")
-	new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
-
-	count = 1
-
-	ports = [
-	]
-
-	for line in ssh_config_file:
-		if count == 55:
-			new_ssh_config_file.write("PasswordAuthentication yes")
-		else:
-			new_ssh_config_file.write(line)
-
-		if "Port" in line:
-			ports.append(line[line.index(" ") + 1:-1])
-		count += 1
-
-	ssh_config_file.close()
-	new_ssh_config_file.close()
-
-	os.system("sudo mv /etc/ssh/sshd_config_2 /etc/ssh/sshd_config")
-	os.system("sudo service ssh restart")
-
-	for port in ports:
-		os.system("sudo iptables -A INPUT -p tcp --dport " + port + " -j DROP")
-
-
 
 
 
