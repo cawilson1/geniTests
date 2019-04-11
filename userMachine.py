70a52ff77c7e47b65d8234eb38468178bafdbc59
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-04-11 18:49:00 -0400

    Update userMachine.py

--- userMachine.py
+++ userMachine.py
@@ -1,39 +1,42 @@
-	os.system("sudo useradd alice")
-	child = pexpect.spawn("sudo passwd alice")
-	child.expect("Enter new UNIX password: ")
-	child.sendline("password")
-	child.expect("Retype new UNIX password: ")
-	child.sendline("password")
-	os.system("sudo chown -R alice /home/alice")
-	os.system("sudo chgrp -R alice /home/alice")
-	
-	
-	ssh_config_file = open("/etc/ssh/sshd_config", "r")
-	new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
+os.system("sudo useradd alice")
+child = pexpect.spawn("sudo passwd alice")
+child.expect("Enter new UNIX password: ")
+child.sendline("password")
+child.expect("Retype new UNIX password: ")
+child.sendline("password")
+os.system("sudo chown -R alice /home/alice")
+os.system("sudo chgrp -R alice /home/alice")
 
-	count = 1
 
-	ports = [
-	]
+ssh_config_file = open("/etc/ssh/sshd_config", "r")
+new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
 
-	for line in ssh_config_file:
-		if count == 55:
-			new_ssh_config_file.write("PasswordAuthentication yes")
-		else:
-			new_ssh_config_file.write(line)
+count = 1
 
-		if "Port" in line:
-			ports.append(line[line.index(" ") + 1:-1])
-		count += 1
+ports = []
 
-	ssh_config_file.close()
-	new_ssh_config_file.close()
+for line in ssh_config_file:
+if count == 54:
+	new_ssh_config_file.write("PasswordAuthentication yes")
+elif count == 33: 
+	new_ssh_config_file.write("RSAAuthentication no")
+elif count == 34:
+	new_ssh_config_file.write("PubkeyAuthentication no")
+else:
+	new_ssh_config_file.write(line)
 
-	os.system("sudo mv /etc/ssh/sshd_config_2 /etc/ssh/sshd_config")
-	os.system("sudo service ssh restart")
+if "Port" in line:
+	ports.append(line[line.index(" ") + 1:-1])
+count += 1
 
-	for port in ports:
-		os.system("sudo iptables -A INPUT -p tcp --dport " + port + " -j DROP")
+ssh_config_file.close()
+new_ssh_config_file.close()
+
+os.system("sudo mv /etc/ssh/sshd_config_2 /etc/ssh/sshd_config")
+os.system("sudo service ssh restart")
+
+for port in ports:
+os.system("sudo iptables -A INPUT -p tcp --dport " + port + " -j DROP")
 
 
 
