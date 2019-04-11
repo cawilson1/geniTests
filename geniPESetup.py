0b61b62fad220b794ab6c542c130321c60c5d9d5
Setzlerte <46764893+Setzlerte@users.noreply.github.com>
2019-04-10 20:53:48 -0400

    Update geniPESetup.py

--- geniPESetup.py
+++ geniPESetup.py
@@ -70,29 +70,28 @@ os.system("sudo wget -P /home/jDoe/Pictures/Wallpapers https://raw.githubusercon
 #preparing the file for SSH
 
 ssh_config_file = open("/etc/ssh/sshd_config", "r")
-	new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
+new_ssh_config_file = open("/etc/ssh/sshd_config_2", "w")
 
-	count = 1
+count = 1
 
-	ports = [
-	]
+ports = []
 
-	for line in ssh_config_file:
-		if count == 55:
-			new_ssh_config_file.write("PasswordAuthentication yes")
-		else:
-			new_ssh_config_file.write(line)
+for line in ssh_config_file:
+	if count == 55:
+		new_ssh_config_file.write("PasswordAuthentication yes")
+	else:
+		new_ssh_config_file.write(line)
 
-		if "Port" in line:
-			ports.append(line[line.index(" ") + 1:-1])
-		count += 1
+	if "Port" in line:
+		ports.append(line[line.index(" ") + 1:-1])
+	count += 1
 
-	ssh_config_file.close()
-	new_ssh_config_file.close()
+ssh_config_file.close()
+new_ssh_config_file.close()
 
-	os.system("sudo mv /etc/ssh/sshd_config_2 /etc/ssh/sshd_config")
-	os.system("sudo service ssh restart")
+os.system("sudo mv /etc/ssh/sshd_config_2 /etc/ssh/sshd_config")
+os.system("sudo service ssh restart")
 
-	for port in ports:
-		os.system("sudo iptables -A INPUT -p tcp --dport " + port + " -j DROP")
+for port in ports:
+	os.system("sudo iptables -A INPUT -p tcp --dport " + port + " -j DROP")
 
